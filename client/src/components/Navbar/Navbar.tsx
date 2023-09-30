5301e47b0647e26ade8ce483f93559bf84f35c7c
Drexregion <96681590+Drexregion@users.noreply.github.com>
2023-09-30 13:04:09 -0400

    Divine updated navbar 3 points
    Performed update on navbar and the sign up and login

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAccountContext } from "../../context";
 import "./Navbar.style.scss";
+import image from '../../../public/logo2.png';
 
 function Navbar() {
   const { loggedIn, logout } = useAccountContext();
@@ -9,14 +10,15 @@ function Navbar() {
 
   return (
     <div className="navbar">
+      <img src= {image } className="navbar_logo"/>
       <div className="navbar__logo">
         <Link to="/">Store Logo</Link>
       </div>
       <div className="navbar__account">
         {loggedIn() === false ? (
           <>
-            <button onClick={() => navigate("/sign-up")}>Sign Up</button>
-            <button onClick={() => navigate("/login")}>Login</button>
+            <button className="navbutton" onClick={() => navigate("/sign-up") }>Sign Up</button>
+            <button className = 'navbutton' onClick={() => navigate("/login")}>Login</button>
           </>
         ) : (
           <button onClick={() => logout()}>Logout</button>
